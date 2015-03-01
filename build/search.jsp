3da4a1102db9362e56eed4644f460937b5b38eab
Dongyang Li <dongyang.li.cs@gmail.com>
2015-03-01 13:41:41 -0800

    autosuggest done

--- build/search.jsp
+++ build/search.jsp
@@ -1,19 +1,32 @@
 <%@ page import = "edu.ucla.cs.cs144.SearchResult" %>
-
+<%
+	String numResultsToSkip = "" + request.getAttribute("numResultsToSkip");
+	String numResultsToReturn = "" + request.getAttribute("numResultsToReturn");
+	String query = "" + request.getAttribute("query");
+%>
 <HTML>
+	<HEAD>
+		<SCRIPT type="text/javascript" src="autosuggest.js"></SCRIPT>
+		<SCRIPT type="text/javascript" src="provider.js"></SCRIPT>
+		<SCRIPT type="text/javascript">
+			window.onload = function () {
+				var oTextbox = new AutoSuggestControl(document.getElementById("qtxt"), new SuggestionProvider());        
+			}
+		</SCRIPT>
+		<LINK rel="stylesheet" href="suggest.css">
+
+	</HEAD>
 	<BODY>
+		<h1>Keyword Search Page</h1>
 		<form action="/eBay/search">
 			<input type="hidden" name="numResultsToSkip" value="0"/>
 			<input type="hidden" name="numResultsToReturn" value="10"/>
 			Search:</br>
-			<input type="text" name="q"/>
+			<input type="text" name="q", id = "qtxt", value="<%= query %>" autocomplete="off" style="width:500px;"/>
 			<input type="submit" value="submit"/>
 		</form>
 
 		<%
-			String numResultsToSkip = "" + request.getAttribute("numResultsToSkip");
-			String numResultsToReturn = "" + request.getAttribute("numResultsToReturn");
-			String query = "" + request.getAttribute("query");
 			String[] result = (String[])request.getAttribute("result");
 			String prev = "" + request.getAttribute("prev");
 			String next = "" + request.getAttribute("next");
--- src/edu/ucla/cs/cs144/ProxyServlet.java
+++ src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -8,12 +8,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+
 public class ProxyServlet extends HttpServlet implements Servlet {
        
     public ProxyServlet() {}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        try{
+
+        	response.setContentType("text/xml"); 
+        	String baseurl = "http://google.com/complete/search?output=toolbar&";
+        	String charset = "UTF-8";
+
+        	String q = request.getParameter("q");
+        	String query = String.format("q=%s", URLEncoder.encode(q, charset));
+        	URLConnection connection = new URL(baseurl + query).openConnection();
+        	connection.setRequestProperty("Accept-Charset", charset);
+
+        	BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        	String inputLine;
+        	StringBuffer buf = new StringBuffer();
+
+        	while ((inputLine = in.readLine()) != null) {
+        		buf.append(inputLine);
+        	}
+
+        	PrintWriter out = response.getWriter();
+
+        	out.println(buf.toString());
+
+        }catch(Exception e) {
+        	response.getWriter().write(e.toString());
+        }
     }
 }
